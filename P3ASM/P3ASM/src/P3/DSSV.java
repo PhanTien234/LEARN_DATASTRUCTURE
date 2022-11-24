4bbf43f1cbcfd6a7df1b27463095954e2bd5f516
PhanTien234 <minhtienp328@gmail.com>
2022-11-24 16:47:55 +0700

    Recursive

--- P3ASM/P3ASM/src/P3/DSSV.java
+++ P3ASM/P3ASM/src/P3/DSSV.java
@@ -1,12 +1,12 @@
 package P3;
 
+import java.util.Scanner;
+
 class SV{
-    public String ID, Name, Address, Phone, Clas;
-    public SV(){}
-    public SV(String ID, String Name, String Address, String Phone, String Clas){
+    public String ID, Name, Phone, Clas;
+    public SV(String ID, String Name, String Phone, String Clas){
         this.ID = ID;
         this.Name = Name;
-        this.Address = Address;
         this.Phone = Phone;
         this.Clas = Clas;
     }
@@ -21,7 +21,7 @@ public class DSSV {
 
     void addFirst(SV x) {
         Node p = new Node();            // tao ra 1 node moi
-        p.data = x;
+        p.data = x; // dua x vao phan data cua p
         p.next = null;
         if (head == null)               // ds rong
             head = tail = p;
@@ -33,15 +33,64 @@ public class DSSV {
         }
     }
 
-    void addLast() {
+    void addLast(SV x)
+    {
+        Node p = new Node();            // tao ra 1 node moi
+        p.data = x; //dua x vao phan data cua p
+        p.next = null;
+        if(head == null) // neu ds rog thi p la node dau cung la cuoi
+            head = tail =p;
+        else {
+            tail.next = p;
+            tail = p;
+        }
+    }
+    void Nhap(){
+        String id, name, phone, cls;
+        int dung = 0;
+        Scanner sc = new Scanner(System.in);
+        while (dung == 0){
+            System.out.print("Enter ID: ");
+            id = sc.nextLine();
+            System.out.print("Enter name: ");
+            name = sc.nextLine();
+            System.out.print("Enter the phone: ");
+            phone = sc.nextLine();
+            System.out.print("Enter the Class: ");
+            cls = sc.nextLine();
+            SV x = new SV(id, name, phone, cls); // Tao 1 SV x
+            addLast(x); // Them SV x vao cuoi danh sach
+            System.out.print("Stop? (1/0) ");
+            dung = Integer.parseInt(sc.nextLine());
+        }
 
     }
+    void TimID(String id) // Tim kiem bang Id
+    {
+        Node p = head;
+        System.out.println("ID\t\t\tName\t\t\tPhone\t\t\tClass");
+        while(p!= null){
+            if(id.compareToIgnoreCase(p.data.ID )== 0){
+                System.out.printf("%2s\t%13s\t", p.data.ID, p.data.Name);
+                System.out.printf("%8s\t%8s\t", p.data.Phone, p.data.Clas);
+                System.out.println();
+            }
+            p=p.next;
+        }
+    }
+    void TimTen(String name) // tim theo ten
+    {}
+    void TImLop(String clas){
+
+    }
+    void delFirst(){}
+    void delLAst(){}
 
     void InDs(){
         Node p = head;
-        System.out.println("ID\t\t\tName\t\t\tPhone\t\t\tClass\t\t\tAddress");
+        System.out.println("ID\t\t\tName\t\t\tPhone\t\t\tClass");
         while(p!= null){
-            System.out.printf("%8s\t%8s\t", p.data.ID, p.data.Name);
+            System.out.printf("%2s\t%13s\t", p.data.ID, p.data.Name);
             System.out.printf("%8s\t%8s\t", p.data.Phone, p.data.Clas);
             System.out.println();
             p = p.next;
@@ -49,11 +98,12 @@ public class DSSV {
     }
     public static void main(String[] args) {
         DSSV DS = new DSSV();
-        SV s1 = new SV("GCD201883", "Paika", "NY", "123", "GCD1001");
-        DS.addFirst(s1);
-        SV s2 = new SV("GCD201883", "Phong", "NY", "123", "GCD1001");
-        DS.addFirst(s2);
+        DS.Nhap();
         DS.InDs();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter ID you want to search: ");
+        String id = sc.nextLine();
+        DS.TimID(id);
     }
 
 }
---
+++ lab4/lab4/src/Recursion/Recursion.java
@@ -0,0 +1,53 @@
+package Recursion;
+
+public class Recursion {
+    int GT(int n){
+        if(n==1)
+            return 1; // base case: DK duwngf
+        else
+            return n * GT(n-1); // Recursive call = Inductive case
+    }
+    int Fibonaci(int n){
+        if(n<= 2){
+            return n;
+        }else {
+            return Fibonaci(n-1) + Fibonaci(n-2);
+        }
+    }
+    void inDayFibo(int n){
+        for(int i =1; i<=n; i++){
+            System.out.print(Fibonaci(i)+ " ");
+        }
+    }
+    void Convert(int n){
+        if (n > 0) {
+            Convert(n/2);
+            System.out.print(n % 2);
+        }else
+            return;
+    }
+    void MoveDisk(int n , char A, char B, char TG) // Chuyen n dia tu A-B
+    {
+        if(n==1){
+            System.out.print("\nMove from " + A + "to "+ B);
+        }else {
+            MoveDisk(n-1, A, TG, B);// 1. Bung n-1 dia tu A => TG, su dung B lam Trung gian
+            System.out.print("\nMOve from "+ A + " to " + B); // 2. Bung 1 dia tu A sang B
+            MoveDisk(n-1, TG, B, A); //3. Bung nguoc n-1 dia tu TG=>B, su dung A lam trung gian
+        }
+    }
+    public static void main(String[] args){
+        Recursion R = new Recursion();
+        int n = 1;
+        System.out.printf("%d! = %d", n, R.GT(n));
+        System.out.println();
+        System.out.printf("Fibo of %d = %d", n, R.Fibonaci(n));
+        System.out.println();
+        System.out.println("Fibo sequence of "+ n +": ");
+        R.inDayFibo(n);
+        System.out.println();
+        System.out.println("Binary of "+ n +": ");
+        R.Convert(n);
+        R.MoveDisk(n, 'A', 'B', 'T');
+    }
+}
